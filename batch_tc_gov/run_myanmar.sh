350e2fca0b128f445229c792de2a6ffc26c77897
Ye <mengqi.ye@vu.nl>
2023-06-19 14:31:26 +0200

    add batch_tc_gov

---
+++ batch_tc_gov/run_myanmar.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBATCH --job-name=run_myanmar
+#SBATCH --partition=ivm
+#SBATCH --nodelist node242
+#SBATCH --ntasks=1
+#SBATCH --time=10-00
+#SBATCH --cpus-per-task=2
+#SBATCH --output=out_myanmar
+#SBATCH --error=err_myanmar
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mengqi.ye@vu.nl
+
+source activate py310
+python /scistor/ivm/mye500/projects/pg_risk_analysis/src/risk.py MMR tc gov
