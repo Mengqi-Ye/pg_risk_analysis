d89feba9513f20395ef45922e4f55d72df1302fc
Ye <mengqi.ye@vu.nl>
2023-06-19 14:30:56 +0200

    add batch_fl_osm

---
+++ batch_fl_osm/run_south_korea_fl_osm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#SBATCH --job-name=run_south_korea
+#SBATCH --partition=ivm
+#SBATCH --nodelist node241
+#SBATCH --ntasks=1
+#SBATCH --time=10-00
+#SBATCH --cpus-per-task=2
+#SBATCH --output=out_south_korea
+#SBATCH --error=err_south_korea
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=mengqi.ye@vu.nl
+
+source activate py310
+python /scistor/ivm/mye500/projects/pg_risk_analysis/src/risk.py KOR fl osm
